147c139f9444315d2109007773ef4a73313c38e2
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2025-10-19 10:49:54 +0200

    building hero overview page

--- app/pages/hero.vue
+++ app/pages/hero.vue
@@ -8,23 +8,19 @@ const { hero, loading, error, fetchHero } = useHero();
 onMounted(() => {
     fetchHero();
 })
-
-function handleCreate() {
-    navigateTo('/create-hero')
-}
 </script>
 
 <template>
+    <h1>Hero Overview</h1>
     <div v-if="loading">
-        <h1>Loading hero..</h1>
+        <h2>Loading hero..</h2>
     </div>
     <div v-else-if="error">
         <p>{{ error }}</p>
     </div>
     <div v-else-if="hero">
-        <h1>Hero Overview</h1>
         <h2>{{ hero.hero_name }}</h2>
-        <button @click="handleCreate">Create new Hero</button>
         <button @click="logoutUser">Sign Out</button>
+        <HeroNav />
     </div>
 </template>
