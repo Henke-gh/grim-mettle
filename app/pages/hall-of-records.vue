d8b9c41f0e8821b454bfb2299a17eb1097c491b5
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2025-11-03 15:15:30 +0100

    added hall of records, complete with api routes, needs navigation and styling

---
+++ app/pages/hall-of-records.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="recordsWrapper">
+
+        <section class="recordsContainer">
+            <h2>The Favoured living</h2>
+            <h3>Those who have clawed their way to the top.</h3>
+            <table class="recordTable" v-if="leaderboard">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Level</th>
+                        <th scope="col">Total XP</th>
+                    </tr>
+                    <tr v-for="hero in leaderboard" :key="hero.id">
+                        <th scope="row">{{ hero.hero_name }}</th>
+                        <td>{{ hero.hero_lvl }}</td>
+                        <td>{{ hero.hero_xp }}</td>
+                    </tr>
+                </thead>
+            </table>
+            <h2>The Graveyard</h2>
+            <h3>Where the fallen are remembered.</h3>
+            <table class="recordTable" v-if="fallenHeroes">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Level</th>
+                        <th scope="col">Total XP</th>
+                    </tr>
+                    <tr v-for="rip in fallenHeroes" :key="rip.id">
+                        <th scope="row">{{ rip.hero_name }}</th>
+                        <td>{{ rip.hero_lvl }}</td>
+                        <td>{{ rip.hero_xp }}</td>
+                    </tr>
+                </thead>
+            </table>
+            <p v-if="errorMsg">{{ errorMsg }}</p>
+        </section>
+    </div>
+    <HeroNav v-if="hero.id" />
+    <CustomFooter v-else />
+</template>
+
+<script setup>
+import { useHeroView } from '#imports';
+import CustomFooter from '~/components/CustomFooter.vue';
+
+const hero = useHeroView();
+const fallenHeroes = ref([]);
+const leaderboard = ref([]);
+const errorMsg = ref('');
+
+async function getLeaderboard() {
+    errorMsg.value = '';
+
+    try {
+        return await $fetch('/api/leaderboard', {
+            method: 'GET'
+        })
+    } catch (err) {
+        errorMsg.value = "No heroes on leaderboard found."
+        return [];
+    }
+}
+
+async function getFallenHeroes() {
+    errorMsg.value = '';
+
+    try {
+        return await $fetch('/api/graveyard', {
+            method: 'GET'
+        })
+
+    } catch (err) {
+        errorMsg.value = "Could not find any graves."
+        return [];
+    }
+}
+
+onMounted(async () => {
+    leaderboard.value = await getLeaderboard();
+    fallenHeroes.value = await getFallenHeroes();
+})
+
+</script>
+
+<style scoped>
+.recordsWrapper {
+    padding: 0.5rem;
+}
+
+.recordsContainer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0;
+}
+</style>
