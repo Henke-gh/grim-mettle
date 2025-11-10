2def59c45d06812c339549a6242ea9daec866f2e
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2025-11-10 14:15:49 +0100

    healing price adjustment

--- utils/healingItems.js
+++ utils/healingItems.js
@@ -3,21 +3,21 @@ export const healingItems = [
     id: 1,
     name: "Bandages",
     healingValue: 10,
-    cost: 20,
+    cost: 15,
     description: "They look mostly clean, mostly.",
   },
   {
     id: 2,
     name: "Forhild's Tonic",
     healingValue: 15,
-    cost: 30,
+    cost: 25,
     description: "The Forhild Special, it's quite rejuvenating!",
   },
   {
     id: 3,
     name: "Liquid Numbness",
     healingValue: 35,
-    cost: 65,
+    cost: 60,
     description:
       "Place a couple of drops under your tounge. You might want to lay down for a bit afterwards.",
   },
@@ -25,7 +25,7 @@ export const healingItems = [
     id: 4,
     name: "Sothian Leaf-mix",
     healingValue: 70,
-    cost: 120,
+    cost: 110,
     description:
       "Produces a thick, brown smoke. This can't be good for your long-term health.",
   },
