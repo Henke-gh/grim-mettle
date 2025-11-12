cf41bc1ad0b3216f95c07110bd2ca03eaf12e17f
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2025-11-12 10:04:32 +0100

    added a result card for hero death combat result

--- app/pages/combat.vue
+++ app/pages/combat.vue
@@ -3,13 +3,18 @@
         <h1>- Combat Log -</h1>
     </header>
     <div class="combatWrapper">
+        <div class="swordsContainer">
+            <img :src="swords" alt="Two crossed swords" class="swordsImg" />
+        </div>
         <section class="logEntry" v-for="(entry, index) in log" :key="index">
             <h2 v-if="entry.type === 'combat_start'">{{ entry.data.hero }} vs {{ entry.data.monster }}</h2>
             <div class="turnBlock" v-if="entry.type === 'turn'">
                 <h3>Turn {{ entry.data.number }}</h3>
                 <article class="turnAction" v-for="(action, idx) in entry.data.actions" :key="idx">
                     <!-- Turn Initiative -->
-                    <p v-if="action.type === 'initiative'">{{ action.data.fighter }} gets the upper hand!</p>
+                    <p v-if="action.type === 'initiative'" class="italic">{{ action.data.fighter
+                        }} gets the upper hand!
+                    </p>
                     <!-- Attack phase -->
                     <p v-else-if="action.type === 'attack'">
                         <template v-if="action.data.hit">
@@ -21,9 +26,9 @@
                                     {{ action.data.defender }} parries the attack with {{ action.data.shield }} avoiding
                                     the worst of it.
                                     {{ action.data.defender }} takes {{ action.data.damage }}
-                                    <span v-if="action.data.damageReduction > 0" class="italic">
+                                    damage!<span v-if="action.data.damageReduction > 0" class="italic">
                                         ({{ action.data.damageReduction }} dmg absorbed)
-                                    </span> damage!
+                                    </span>
                                 </template>
                                 <template v-else>
                                     {{ action.data.attacker }} charges towards {{ action.data.defender }} with {{
@@ -31,9 +36,9 @@
                                     {{ action.data.defender }} tries to block the hit with {{ action.data.shield }} but
                                     isn't quick enough.
                                     {{ action.data.attacker }} strikes a clean blow dealing {{ action.data.damage }}
-                                    <span v-if="action.data.damageReduction > 0" class="italic">
+                                    damage!<span v-if="action.data.damageReduction > 0" class="italic">
                                         ({{ action.data.damageReduction }} dmg absorbed)
-                                    </span> damage!
+                                    </span>
                                 </template>
                             </template>
 
@@ -43,9 +48,9 @@
                                     {{ action.data.attacker }} charges towards {{ action.data.defender }} with {{
                                         action.data.weapon }}.
                                     {{ action.data.attacker }} strikes a clean blow dealing {{ action.data.damage }}
-                                    <span v-if="action.data.damageReduction > 0" class="italic">
+                                    damage!<span v-if="action.data.damageReduction > 0" class="italic">
                                         ({{ action.data.damageReduction }} dmg absorbed)
-                                    </span> damage!
+                                    </span>
                                 </template>
                                 <template v-else>
                                     {{ action.data.attacker }} charges towards {{ action.data.defender }} with {{
@@ -66,26 +71,41 @@
 
                     <!-- Combat Resolves -->
                     <p v-else-if="action.type === 'fatigue'">
-                        {{ action.data.fighter }} collapses in the sand, too tired to keep fighting.
+                        <span class="italic">{{ action.data.fighter }} collapses in the sand, too tired to keep
+                            fighting.</span>
                     </p>
                     <p v-else-if="action.type === 'defeat'">
-                        {{ action.data.defeated }} {{ action.data.slain ? 'is slain!' : 'is defeated.' }}
+                        <span class="italic">
+                            {{ action.data.defeated }} {{ action.data.slain ? 'is slain!' : 'is defeated.' }}
+                        </span>
                     </p>
                 </article>
             </div>
             <!-- Rewards, retreats or deaths -->
             <div v-else-if="entry.type === 'combat_end'">
-                <p v-if="entry.data.result === 'victory'">{{ entry.data.hero }} is victorious!</p>
-                <div v-if="entry.data.rewards">You earn {{ entry.data.rewards.xp }} XP and {{ entry.data.rewards.gold }}
-                    gold from the
-                    duke's coffers.</div>
-                <p v-else-if="entry.data.result === 'death'">Your journey ends. The dead salute you and the living move
-                    on.</p>
+                <p v-if="entry.data.result === 'victory'"><span class="bold">{{ entry.data.hero }}</span> is victorious!
+                </p>
+                <div v-if="entry.data.rewards">You earn <span class="bold">{{ entry.data.rewards.xp }} XP</span> and
+                    <span class="bold">{{
+                        entry.data.rewards.gold }}
+                        gold</span> from the
+                    duke's coffers.
+                </div>
+                <div v-else-if="entry.data.result === 'death'" class="resultCard">
+                    <img :src="death" alt="Skulls and weapons piled around each other. Your hero has died."
+                        class="heroDeathImg" />
+                    <div class="resultText">
+                        <p class="bold">Your journey ends.</p>
+                        <p>The dead salute you and the living move on.</p>
+                    </div>
+                </div>
                 <p v-else>You live to fight another day, tend to your wounds.</p>
             </div>
         </section>
         <button class="defaultButton default" @click="exitCombatLog">Continue</button>
     </div>
+    <div class="swordlineContainer spacing"><img src="/divider.svg"
+            alt="A line of four swords, with a shield in the middle" /></div>
     <HeroNav />
 </template>
 
@@ -93,6 +113,10 @@
 definePageMeta({
     middleware: ["auth",],
 });
+import swords from "../assets/images/crossedSwords.png"
+import death from "../assets/images/player_death_small.png"
+
+
 const combatResult = useCombatResult();
 const log = combatResult.combatLog.value;
 
@@ -134,6 +158,37 @@ function exitCombatLog() {
     display: flex;
     flex-direction: column;
     align-items: center;
+}
+
+.swordsImg {
+    height: 5rem;
+    width: auto;
+}
+
+.resultCard {
+    display: flex;
+    flex-direction: row;
+    align-items: end;
+    gap: 0.5rem;
+    margin-bottom: 0.5rem;
+}
+
+.heroDeathImg {
+    height: 6rem;
+    width: auto;
+}
+
+.resultText {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    margin-bottom: 0.5rem;
+}
+
+.turnBlock {
+    display: flex;
+    flex-direction: column;
+    gap: 0.4rem;
 }
 
 .defaultButton {
