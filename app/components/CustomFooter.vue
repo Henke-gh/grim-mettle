a1e972a541ef7089a87901a08a47dfeeed5bcc92
Henke-gh <77665143+Henke-gh@users.noreply.github.com>
2026-05-12 11:45:59 +0200

    changed footer colour scheme

--- app/components/CustomFooter.vue
+++ app/components/CustomFooter.vue
@@ -17,8 +17,8 @@ footer {
     padding-top: 1rem;
     padding-bottom: 1rem;
     width: 100%;
-    background-color: var(--dark-purple);
-    color: var(--yellow);
+    background-color: var(--brown);
+    color: var(--bone-white);
     text-align: center;
     border-top: 4px double var(--bone-white);
 }
